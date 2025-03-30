be242c67f469fe291931082358fe2e96b4ba0217
Siddharth Kumar <roysiddharth458@gmail.com>
2025-03-30 14:05:05 +0530

    Day 1

--- Array/SecondLargest.java
+++ Array/SecondLargest.java
@@ -14,7 +14,7 @@ public class SecondLargest{
         for(int i=0; i<arr.length ; i++){
             if(arr[i]>max){
                 max=arr[i];
-            }
+            } 
         }
         for(int i=0; i<arr.length ; i++){
             if(arr[i]>smax && arr[i]<max){
