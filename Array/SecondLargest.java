28add2efdf13346bf22545337e71554cb8487c2e
Siddharth Kumar <roysiddharth458@gmail.com>
2025-04-01 13:23:20 +0530

    second largest element in an array

--- Array/SecondLargest.java
+++ Array/SecondLargest.java
@@ -13,7 +13,7 @@ public class SecondLargest{
         
         for(int i=0; i<arr.length ; i++){
             if(arr[i]>max){
-                max=arr[i];
+                max=arr[i]; 
             } 
         }
         for(int i=0; i<arr.length ; i++){
